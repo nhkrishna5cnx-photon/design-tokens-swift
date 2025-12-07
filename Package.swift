dee566c196298cb3cc3c1a006fe3d0088316cf98
Hari Krishna <nhkrishna5.cnx@gmail.com>
2025-12-07 20:40:06 +0530

    updated package swift file 1.1

--- Package.swift
+++ Package.swift
@@ -10,18 +10,6 @@ let package = Package(
     ],
     
     products: [
-        // ✅ Public Tokens Module
-        .library(
-            name: "DesignTokens",
-            targets: ["DesignTokens"]
-        ),
-        
-        // ✅ Public Utilities Module
-        .library(
-            name: "Utilities",
-            targets: ["Utilities"]
-        ),
-        
         // ✅ Public Button Component Module
         .library(
             name: "GSKButtonComponent",
@@ -30,25 +18,8 @@ let package = Package(
     ],
     
     targets: [
-        // ✅ Design Tokens Target
-        .target(
-            name: "DesignTokens",
-            path: "Sources/DesignTokens"
-        ),
-        
-        // ✅ Utilities Target
-        .target(
-            name: "Utilities",
-            path: "Sources/Utilities"
-        ),
-        
-        // ✅ Button Component Target (Depends on Tokens + Utilities)
         .target(
             name: "GSKButtonComponent",
-            dependencies: [
-                "DesignTokens",
-                "Utilities"
-            ],
             path: "Sources/GSKButtonComponent"
         )
     ]
